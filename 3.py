ca96c5bf0cc1a4215cc5ef1976bafc22f942ce6f
pavani2433 <146101313+pavani2433@users.noreply.github.com>
2025-01-25 23:56:41 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,19 +1,15 @@
 import random
-
-def guess_the_number():
-    number_to_guess = random.randint(1, 100)
-    attempts = 0
-
+def guessthenumber():
+    numbertoguess = random.randint(1, 100)
+    att= 0
     while True:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
-
-        if guess < number_to_guess:
-            print("Too Low! Try again.")
-        elif guess > number_to_guess:
-            print("Too High! Try again.")
+        guess=int(input("Enter your guess number:"))
+        att=att+1
+        if guess<numbertoguess:
+            print("Too Low! Try again")
+        elif guess>numbertoguess:
+            print("Too High! Try again")
         else:
-            print(f"Congratulations! You've guessed the number {number_to_guess} correctly in {attempts} attempts.")
+            print(f"Congratulations! You have guessed the number correctly in {att} attempts.")
             break
-
-guess_the_number()
+guessthenumber()
